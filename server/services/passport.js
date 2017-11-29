9a7d96952bf6bc1220a0fc7979b5c75b098f2b29
Tee <bunth667@gmail.com>
2017-11-29 13:04:29 -0500

    removed proxy:true for auth/google/callback

--- server/services/passport.js
+++ server/services/passport.js
@@ -2,7 +2,6 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
-
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
@@ -20,7 +19,7 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback'        
     },
         (accessToken, refreshToken, profile, done) => { 
             User.findOne({ googleId: profile.id })
@@ -36,4 +35,3 @@ passport.use(
         }
     )
 );
-
