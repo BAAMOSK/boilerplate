e03b16bed796a3f34b01af3a8b03e0279d8a7d6f
Tee <bunth667@gmail.com>
2017-11-30 22:06:22 -0500

    react-router working

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>
+const Profile = () => <h2>Profile</h2>
+const Contact = () => <h2>Contact</h2>
+const About = () => <h2>About</h2>
 
 const App = () => {
     return (
         <div>
-            Hi There
+            <BrowserRouter>
+                <div>
+                    <Route path="/" component={Profile} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
