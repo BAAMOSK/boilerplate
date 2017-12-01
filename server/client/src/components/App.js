180615a2659b887b12a244585d00c589c9f73dda
Tee <bunth667@gmail.com>
2017-11-30 22:57:41 -0500

    added proxy call to client for all api requests

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>
+import Header from './Header';
 const Profile = () => <h2>Profile</h2>
 const Contact = () => <h2>Contact</h2>
 const About = () => <h2>About</h2>
@@ -10,8 +10,11 @@ const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <div>
-                    <Route path="/" component={Profile} />
+                <div className="container">
+                    <Header />
+                    <Route exact path="/" component={Profile} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Route path="/contact/about" component={About} />
                 </div>
             </BrowserRouter>
         </div>
